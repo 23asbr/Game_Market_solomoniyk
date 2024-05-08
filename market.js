fe653a8072b13197041f695cf1a844ad88b95c02
23asbr <23a_sbr@liceum.ztu.edu.ua>
2024-05-08 09:43:11 +0300

    array

--- market.js
+++ market.js
@@ -3,16 +3,28 @@ console.log('test')
 let itemsDiv = document.getElementById("items");
 
 if(itemsDiv){
-    console.log(itemsDiv)
-    console.log('Поле classList: ', itemsDiv.classList)
-    console.log('Поле id: ', itemsDiv.id)
-    console.log('Поле clientWidth: ', itemsDiv.clientWidth)
-    console.log('Поле innerHTML: ', itemsDiv.innerHTML)
-    for(let i=0; i<=100; i++){
-        itemsDiv.innerHTML = '<div class = item></div>'
-    }
+    //for(let i=0; i<=100; i++){
+    //    itemsDiv.innerHTML = '<div class = item></div>'
+    //}
     
     
 } else{
     console.log("not found")
 }
+
+let itemsArray = [
+    'Газонокосарка 43',
+    'Електричний тример 110',
+    'Електрична газонокосарка 32',
+    'Акумуляторний оприскувач 12',
+]
+console.log(itemsArray)
+
+itemsArray = itemsArray.sort().forEach((item) =>{
+    console.log(item)
+}
+)
+
+//for(let i =0; i<itemsArray.length; i++){
+//    console.log(i + '-й елемент: ', itemsArray[i])
+//}
